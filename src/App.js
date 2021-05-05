f5d45a34974162c1efc54fd5afadfd8eb34cdd5d
Jose Garcia <jpgarciap1997@gmail.com>
2021-05-04 21:22:52 -0600

    adding states in app.js to display in the page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React, {useState} from 'react'
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY = [
+  { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+  { title: "Car Insurance", amount: 30.67, date: new Date(2021, 2, 27) }
+];
+
 function App() {
-  const expenses = [
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: "Car Insurance", amount: 30.67, date: new Date(2021, 2, 27) }
-  ];
+
+  const [expenses, setExpenses] = useState(DUMMY);
+  
 
   const addExpenseHandler = expense => {
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
   }
   return (
     <div className="App">
