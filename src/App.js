5b90343af1196556432c5c1e01b94198ee3b33fe
Jose Garcia <jpgarciap1997@gmail.com>
2021-05-04 21:28:29 -0600

    adding key and ids to the dummy data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from './components/NewExpense/NewExpense';
 
 const DUMMY = [
-  { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
-  { title: "Car Insurance", amount: 30.67, date: new Date(2021, 2, 27) }
+  { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) , id:'e1'},
+  { title: "Car Insurance", amount: 30.67, date: new Date(2021, 2, 27), id:'e2' }
 ];
 
 function App() {
